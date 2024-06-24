cbf7caf21720f577a770da13f1f748021f45f730
alan <alanwengrzee@gmail.com>
2024-06-24 15:28:39 -0300

    add register users

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
-import { FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
-import { Link } from "react-router-dom";
+import { api } from "../../services/api";
+
+import { Link, useNavigate } from "react-router-dom";
 
 export function SignUp(){
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  function handleSignUp(){
+    if(!name || !email || !password){
+      alert("Preencha todos os campos");
+      return;
+    }
+    api.post("/users", { name, email, password })
+    .then(()=>{
+      alert("Usário criado com sucesso");
+      navigate("/");
+    })
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message)
+      }else{
+        alert("Não foi possivel criar o usário")
+      }
+    })
+  }
+
   return(
     <Container>
       <Background />
@@ -19,21 +46,27 @@ export function SignUp(){
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button 
+          title="Cadastrar"
+          onClick={handleSignUp}
+        />
 
         <Link to="/">Voltar para o login</Link>
       </Form>
