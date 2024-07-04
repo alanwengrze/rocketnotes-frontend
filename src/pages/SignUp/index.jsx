1fa827556fe5c59f83ee4f5dbb9580e016b660cb
alan <alanwengrzee@gmail.com>
2024-07-04 18:41:24 -0300

    :sparkles: feat: regex email and password

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -14,12 +14,23 @@ export function SignUp(){
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
+
   const navigate = useNavigate();
   function handleSignUp(){
     if(!name || !email || !password){
       alert("Preencha todos os campos");
       return;
     }
+    if(!emailRegex.test(email)){
+      return alert("E-mail inválido. Digite um e-mail válido. Ex: yourname@example.com");
+    }
+    if(!passwordRegex.test(password)){
+      return alert("Senha fraca. Sua senha deve conter pelo menos 8 caracteres, 1 maiúscula, 1 minúscula, 1 número e 1 caractere especial");
+    }
+
     api.post("/users", { name, email, password })
     .then(()=>{
       alert("Usário criado com sucesso");
@@ -51,7 +62,7 @@ export function SignUp(){
 
         <Input 
           placeholder="E-mail"
-          type="text"
+          type="email"
           icon={FiMail}
           onChange={e => setEmail(e.target.value)}
         />
